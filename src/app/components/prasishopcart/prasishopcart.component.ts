a27e99f7cfa999fc1bb3e18cc26b9dd0a1df1b4d
root <root@iPhone>
2020-02-26 13:07:09 -0400

    cart and destinos

--- src/app/components/prasishopcart/prasishopcart.component.ts
+++ src/app/components/prasishopcart/prasishopcart.component.ts
@@ -23,6 +23,47 @@ export class PrasishopcartComponent implements OnInit {
     public _uw:UserWService
   	) { }
 
+   public envio = 0;
+   public destino ='Seleccione un destino';
+
+ localidades: string[]=[
+  'Arica, Iquique',
+  'Antofagasta',
+  'Calama',
+  'Copiapo',
+  'La Serena, Coquimbo',
+  'Valparaiso, Viña del mar',
+  'Santiago',
+  'Rancagua; San Fernando',
+  'Curico, Talca, Chillan, Concepción, Los Angeles',
+  'Temuco, Valdivia, Osorno',
+  'Puerto Montt, Pto. Varas, Castro',
+  'Coyhaique',
+  'Punta Arenas'
+  ];
+	costosEnvio: number[]=[
+	8140,
+	7210,
+	8330,
+	4510,
+	4400,
+	4110,
+	2830,
+	4270,
+	4360,
+	4450,
+	4930,
+	8050,
+	8320  
+	];
+
+setEnvio(){
+	for (var i = 0; i < this.costosEnvio.length; i++) {
+		if (this.destino==this.localidades[i]){
+			this.envio=this.costosEnvio[i];
+		}
+	}
+}
   ngOnInit() {
   }
 
